8793626d370bbbfc42a7dc74a42ad9e7294d1de2
kevinlin91 <kevinlin91@yahoo.com.tw>
2018-04-26 11:21:54 +0800

    scalar update

--- util.py
+++ util.py
@@ -6,9 +6,10 @@ def standard_scaler(X_train, X_test):
     train_samples, train_nx, train_ny = X_train.shape
     test_samples, test_nx, test_ny = X_test.shape
     
-    X_train = X_train.reshape((train_samples, train_nx * train_ny))
-    X_test = X_test.reshape((test_samples, test_nx * test_ny))
-    
+    #X_train = X_train.reshape((train_samples, train_nx * train_ny))
+    #X_test = X_test.reshape((test_samples, test_nx * test_ny))
+    X_train = X_train.reshape((train_samples * train_nx, train_ny))
+    X_test = X_test.reshape((test_samples * test_nx, test_ny))
     preprocessor = prep.StandardScaler().fit(X_train)
     X_train = preprocessor.transform(X_train)
     X_test = preprocessor.transform(X_test)
@@ -16,7 +17,7 @@ def standard_scaler(X_train, X_test):
     X_train = X_train.reshape((train_samples, train_nx, train_ny))
     X_test = X_test.reshape((test_samples, test_nx, test_ny))
     
-    return X_train, X_test
+    return X_train, X_test, preprocessor
 
 
 def preprocess_data(etc_data, seq_len,number_features):
@@ -29,18 +30,20 @@ def preprocess_data(etc_data, seq_len,number_features):
     
     train = result[: int(row), :]
     
-    train, result = standard_scaler(train, result)
+    train, result, preprocessor = standard_scaler(train, result)
     
     X_train = train[:, : -1]
-    y_train = train[:, -1][: ,-2]
+    #y_train = train[:, -1][: ,-2]
+    y_train = train[:,-1]
     X_test = result[int(row) :, : -1]
-    y_test = result[int(row) :, -1][ : ,-2]
+    #y_test = result[int(row) :, -1][ : ,-2]
+    y_test = result[int(row) :, -1]
 
 
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], number_features))
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], number_features))  
 
-    return [X_train, y_train, X_test, y_test]
+    return [X_train, y_train, X_test, y_test, preprocessor]
 
 def score(x_test, y_test, model):
     Score = model.evaluate(x_test, y_test, verbose=0)
@@ -48,3 +51,6 @@ def score(x_test, y_test, model):
 
 def predict(x_test, model):
     return model.predict(x_test)
+
+def inverse_scaler(X_test, preprocessor):
+    return preprocessor.inverse_transform(X_test)
